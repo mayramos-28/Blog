c0d6aa3485c723f4be1562baa6180a6cba3f9a5f
mayra28mos <mayra28mos@gmail.com>
2023-07-01 20:52:28 +0200

    add tables

---
+++ database/migrations/2023_07_01_184021_create_preparations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('preparations', function (Blueprint $table) {
+            $table->id();
+            $table->string('step_number');
+            $table->string('step_description');
+            $table->unsignedBigInteger('post_id')->nullable(false);
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('preparations');
+    }
+};
