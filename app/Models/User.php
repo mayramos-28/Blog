7f80c4064b9a18e1019d7d51a94ab6b5803a4519
mayra28mos <mayra28mos@gmail.com>
2023-07-01 21:32:42 +0200

    modify model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,18 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    public function favorites()
+    {
+        return $this->belongsToMany(Post::class, 'favorites', 'user_id', 'post_id')->withTimestamps();
+    }
 }
