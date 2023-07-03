3cd721ba6d86fcadc0fea604aba0b73c3f72d819
mayra28mos <mayra28mos@gmail.com>
2023-07-03 21:00:48 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        cors: '*',
+        host: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
