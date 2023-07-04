fff393cdbd395b1ca64273f465abb15812acb4a0
mayra28mos <mayra28mos@gmail.com>
2023-07-04 17:37:54 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,10 +18,12 @@ Route::get('/', function () {
     return redirect(route("posts.index"));
 });
 
-
-
 Route::group(['prefix' => 'posts'], function () {
-    Route::get('/', [PostController::class, 'index'])->name('posts.index');   
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
     Route::get('show/{id}', [PostController::class, 'show'])->name('posts.show');
-   
+    Route::post('create', [PostController::class, 'create'])->name('posts.create');
+    Route::get('getCreate', [PostController::class, 'getCreate'])->name('posts.getCreate');
+    Route::post('update/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('getUpdate/{id}', [PostController::class, 'getUpdate'])->name('posts.getUpdate');
+    Route::get('delete/{id}', [PostController::class, 'delete'])->name('posts.delete');
 });
