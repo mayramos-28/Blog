69a71a99a7b38ade3d52ce2e198e2ec7a1106fa0
mayra28mos <mayra28mos@gmail.com>
2023-07-04 12:55:58 +0200

    modify controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+
+        $categoryId = $request->get('category_id');
+        if ($categoryId) {
+            $posts = Post::where('category_id', $categoryId)->get();
+        } else {
+            $posts = Post::all();
+        }
+        return view('pages.post-page', ['posts'=> $posts]);
+    }
+
+    public function show(int $id) {
+        $post = Post::findOrFail($id);
+        return view('pages.post-page', compact('post'));
+    }
 }
