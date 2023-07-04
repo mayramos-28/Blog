6ad5bd8c295e1bd867f0f7b6caf943935cd63cc9
mayra28mos <mayra28mos@gmail.com>
2023-07-04 12:55:09 +0200

    add boostrap

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     server: {
         cors: '*',
-        host: true,
+        host: true
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    alias: {
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
 });
