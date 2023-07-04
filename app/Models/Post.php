e5112d926380da73924502ddfa05c4ffa2543cba
mayra28mos <mayra28mos@gmail.com>
2023-07-04 12:56:14 +0200

    modify post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,16 +10,12 @@ class Post extends Model
     use HasFactory;
     protected $guarded = []; // esto en un entorno real no lo haría, pero para simplificar la prueba lo dejo así
 
-    public function user(){
-        return $this->belongsTo(User::class);
-    }
+  
     public function comments(){
         return $this->morphMany(Comment::class, 'commentable');
     }   
     public function files(){
         return $this->morphMany(File::class, 'fileable');
     }
-    public function favorite(){
-        return $this->hasMany(Favorite::class);
-    }
+    
 }
